709c41956bbbb02343932741b62488d7439d466b
lile <lile@sysware.com.cn>
2023-04-10 10:42:37 +0800

    Release command adjustment

--- scripts/release.js
+++ scripts/release.js
@@ -1,14 +1,13 @@
 'use strict';
 const args = process.argv.slice(2)
 const message = args[0]
-const command = args[1]
 
 const handleVersionAndTime = require('./tools/handleVersionAndTime');
 const handleReleaseGit = require('./tools/handleReleaseGit');
 
 
 handleVersionAndTime(message)
-  .then((res) => handleReleaseGit(command, message))
+  .then((res) => handleReleaseGit(message))
   .then((res) => {
   })
   .catch(err => {
@@ -16,5 +15,4 @@ handleVersionAndTime(message)
   });
 
 
-  // npm run release xxxxxx push
-  // push可选,不选不自动处理
+// npm run release xxxxxx
--- scripts/tools/handleReleaseGit.js
+++ scripts/tools/handleReleaseGit.js
@@ -2,24 +2,31 @@
 const shell = require('shelljs');
 const rootPath = process.cwd();
 const chalk = require('react-dev-utils/chalk');
+const inquirer = require("inquirer");
 
-
-module.exports = function handleReleaseGit(cmd, msg) {
+module.exports = function handleReleaseGit(msg) {
 
   return new Promise((resolve, reject) => {
 
-    if (cmd === "push") {
-      shell.cd(rootPath);
-      shell.exec(`git commit -am "${msg}"`);
-      shell.exec(`git push`);
-      console.log(chalk.yellow(`handleReleaseGit commit & push`));
-      resolve("commit")
-
-    } else {
-      console.log(chalk.yellow(`handleReleaseGit No action`));
-      resolve("ReleaseGit不做操作")
-    }
-
+    inquirer.prompt([
+      {
+        type: "confirm",
+        name: "isCommitPush",
+        message: "是否提交并推送代码",
+      },
+    ]).then((answers) => {
+      if (answers.isCommitPush) {
+        shell.cd(rootPath);
+        shell.exec(`git commit -am "${msg}"`);
+        shell.exec(`git push`);
+        console.log(chalk.yellow(`handleReleaseGit commit & push`));
+        resolve("commit & push")
+      } else {
+        console.log(chalk.yellow(`handleReleaseGit No action`));
+        resolve("ReleaseGit不做操作")
+      }
+    }).catch((error) => {
+      reject(error);
+    });
   })
-
 };
