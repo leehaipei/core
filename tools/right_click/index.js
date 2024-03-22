c168814eb1c07719f65a255b1e2dc16ac491b0f1
lile <lile@sysware.com.cn>
2024-03-22 16:10:17 +0800

    Simple Right Click

---
+++ tools/right_click/index.js
@@ -0,0 +1,78 @@
+import ReactDOM from "react-dom";
+
+export default class RightClick {
+    constructor(querySelector, content) {
+
+        this.trigger = false;
+        this.querySelector = `r-${querySelector}`;
+        const dom = document.querySelector(querySelector);
+        this.dom = dom;
+        this.content = content;
+
+        this.install();
+    };
+
+    install() {
+        if (!this.dom) {
+            console.error(`未找到容器`);
+            return
+        }
+
+        this.dom.addEventListener('contextmenu', (e) => {
+
+            if (!this.trigger) {
+                this.creat();
+            } else {
+                this.remove();
+                this.creat();
+            }
+
+            e.preventDefault();
+        })
+
+        document.addEventListener('click', (e) => {
+            this.remove();
+            e.preventDefault();
+        })
+    };
+
+    creat() {
+        function getMousePos(event) {
+            var esss = event || window.event;
+            var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+            var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+            var x = esss.pageX || esss.clientX + scrollX;
+            var y = esss.pageY || esss.clientY + scrollY;
+            return { 'top': y + 'px', 'left': x + 'px' };
+        }
+        const pos = getMousePos();
+
+        let mask = document.createElement("div");
+        mask.id = `right_click_mask_${this.querySelector}`;
+        mask.style.position = "fixed";
+        mask.style.top = pos.top;
+        mask.style.left = pos.left;
+
+        document.body.appendChild(mask);
+        ReactDOM.render(this.content,
+            document.getElementById(`right_click_mask_${this.querySelector}`)
+        );
+        this.trigger = true;
+    };
+
+    remove() {
+        const mask = document.getElementById(`right_click_mask_${this.querySelector}`);
+        if (!mask) return
+
+        ReactDOM.unmountComponentAtNode(document.getElementById(`right_click_mask_${this.querySelector}`));
+        mask.remove();
+        this.trigger = false;
+    };
+
+    uninstall() {
+        this.remove();
+        this.dom.removeEventListener('contextmenu')
+        document.removeEventListener('click')
+    };
+
+}
