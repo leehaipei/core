9d46b2daf4015e694e1ecd19f3f6593e643f5a4b
lile <lile@e-lead.cn>
2024-12-30 17:28:45 +0800

    Add content for publishing push notifications

--- scripts/push.js
+++ scripts/push.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const moment = require('moment');
 const fs = require('fs-extra');
 const _packageJson = fs.readFileSync('./package.json')
 const _release_record = fs.readFileSync('./release-record.json')
@@ -12,13 +13,14 @@ const buildtime = JSON.parse(_buildtime)
 
 const device_key = args[0]
 const push_host = args[1]
+const action_start = args[2]
 
 // 创建一个新的 Date 对象，并使用 toLocaleString() 方法将其转换为北京时间字符串
 let beijingTimeString = new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai', hour12: false });
 
 axios.post(push_host, {
   "device_key": device_key,
-  "body": `post:${beijingTimeString}\nmessage:${release_record[0].message}\nbuilt:${buildtime.use}\nrelease:${release_record[0].time}`,
+  "body": `post:${beijingTimeString}\nmessage:${release_record[0].message}\nbuilt:${buildtime.use}\nrelease:${release_record[0].time}\naction run:${moment(action_start).diff(moment(), 'seconds', true)}`,
   "title": `${packageJson.name} version:${packageJson.version}`,
   "icon": "https://static.leehaipei.com/images/github.png",
   "group": 'ReleaseNotice'
