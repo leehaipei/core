f88ef8ed363af63ec1693e07451321732a622619
lile <lile@e-lead.cn>
2025-01-08 12:23:28 +0800

    Add custom log style

--- scripts/tools/handleCustomLog.js
+++ scripts/tools/handleCustomLog.js
@@ -7,8 +7,9 @@ const packageJson = JSON.parse(_packageJson)
 function makeCustomLog(isBuild) {
     const nowStr = new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai', hour12: false });
     if (isBuild) {
-        return `console.log("%cversion:${packageJson.version}%c${packageJson["last-release-time"]}","background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a","color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.log("%cbuilt%c${nowStr}","background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a","color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");`
+        return `console.log("%cversion%c${packageJson.version}", "background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
+        console.log("%crelease%c${packageJson['last-release-time']}", "background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
+        console.log("%cbuilt%c${nowStr}", "background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");`
     } else {
         return ""
     }
