f57ecaa9b45e769dda9543f6d90e9a65c701fa40
lile <lile@e-lead.cn>
2025-01-08 09:45:23 +0800

    fix time zone

--- scripts/tools/handleCustomLog.js
+++ scripts/tools/handleCustomLog.js
@@ -1,14 +1,14 @@
 const fs = require('fs-extra');
 const chalk = require('react-dev-utils/chalk');
-const moment = require('moment');
 const _packageJson = fs.readFileSync('./package.json')
 
 const packageJson = JSON.parse(_packageJson)
 
 function makeCustomLog(isBuild) {
+    const nowStr = new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai', hour12: false });
     if (isBuild) {
         return `console.log("%cversion:${packageJson.version}%c${packageJson["last-release-time"]}","background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a","color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.log("%cbuilt%c${moment().format('YYYY-MM-DD HH:mm:ss')}","background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a","color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");`
+        console.log("%cbuilt%c${nowStr}","background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a","color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");`
     } else {
         return ""
     }
