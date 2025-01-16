e42e815d491bd31b934bde70cabb4bd03810722a
lile <lile@e-lead.cn>
2025-01-16 16:07:44 +0800

    custom log style

--- scripts/tools/handleCustomLog.js
+++ scripts/tools/handleCustomLog.js
@@ -8,9 +8,10 @@ const packageJson = JSON.parse(_packageJson)
 function makeCustomLog(isBuild) {
     const nowStr = momentBeijing().format('YYYY-MM-DD HH:mm:ss')
     if (isBuild) {
-        return `console.log("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
+        return `console.groupCollapsed("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
         console.log("%crelease%c${packageJson['last-release-time']}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.log("%cbuilt%c${nowStr}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");`
+        console.log("%cbuilt%c${nowStr}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
+        console.groupEnd();`
     } else {
         return ""
     }
