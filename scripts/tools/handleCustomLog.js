ccc29ec9b6a319134bdd6b310b1e3d3b825751e7
lile <lile@e-lead.cn>
2025-01-16 18:13:30 +0800

    add log core info

--- scripts/tools/handleCustomLog.js
+++ scripts/tools/handleCustomLog.js
@@ -1,25 +1,32 @@
 const fs = require('fs-extra');
+const axios = require('axios');
+const moment = require('moment');
 const chalk = require('react-dev-utils/chalk');
 const _packageJson = fs.readFileSync('./package.json')
 const momentBeijing = require('../../tools/momentBeijing')
 
 const packageJson = JSON.parse(_packageJson)
 
-function makeCustomLog(isBuild) {
+function makeCustomLog(isBuild, coreTime) {
     const nowStr = momentBeijing().format('YYYY-MM-DD HH:mm:ss')
-    if (isBuild) {
-        return `console.groupCollapsed("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.log("%crelease%c${packageJson['last-release-time']}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.log("%cbuilt%c${nowStr}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");
-        console.groupEnd();`
+    if (isBuild && coreTime) {
+        return `console.groupCollapsed("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%crelease%c${packageJson['last-release-time']}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%cbuilt%c${nowStr}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%ccore%c${coreTime}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.groupEnd();`
     } else {
         return ""
     }
 }
 
 module.exports = function handleCustomLog(isBuild = false) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile("./core/tools/console_log/index.js", makeCustomLog(isBuild), (err) => {
+    return new Promise(async (resolve, reject) => {
+
+        let coreBeijingTime = false;
+        if (isBuild) {
+            const commits = await axios.get('https://api.github.com/repos/leehaipei/core/commits')
+            const coreTime = commits?.data?.[0]?.commit?.author?.date;
+            coreBeijingTime = moment.utc(coreTime).utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
+        }
+
+        fs.writeFile("./core/tools/console_log/index.js", makeCustomLog(isBuild, coreBeijingTime), (err) => {
             if (err) {
                 console.log(chalk.red(`custom Log ${isBuild ? "add" : "restore"} error`));
             }
