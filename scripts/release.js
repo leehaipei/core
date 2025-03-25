903b3839ab3039a4abe885e5ae5379dad5122df1
lile <lile@e-lead.cn>
2025-03-25 10:53:44 +0800

    refactor(release.js): style

--- scripts/release.js
+++ scripts/release.js
@@ -13,30 +13,9 @@ async function main() {
     return
   }
 
-
   await handleVersionAndTime(message);
   await handleReleaseGit(message);
 
-
 }
-
-// const handleVersionAndTime = require('./tools/handleVersionAndTime');
-// const handleReleaseGit = require('./tools/handleReleaseGit');
-
-
-// handleVersionAndTime(message)
-//   .then((res) => handleReleaseGit(message))
-//   .then((res) => {
-//   })
-//   .catch(err => {
-//     console.log("release.js", err);
-//   });
-
-// 三位版本号，最后两位通过选择来控制。录入的信息还是通过命令行的参数传入
-// 第一位版本号，必须手动修改
-// 第二位版本号，通过选择来控制。选择后，第二位版本号加1，第三位版本号重置为0。
-// 第三位版本号，通过选择来控制
-
-
 
 main();
