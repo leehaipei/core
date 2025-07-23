8de622e55195c3d035853340783d60b098f63745
lile <lile@e-lead.cn>
2025-07-23 10:33:54 +0800

    style: fix indentation and quote consistency in custom-log-version plugin

--- vite/plugins/custom-log-version.ts
+++ vite/plugins/custom-log-version.ts
@@ -3,13 +3,13 @@ import handleCustomVersionLog from "../../handler/handleCustomVersionLog";
 export default function customLogVersion(): any {
   return {
     name: "custom-log-version",
-   async transformIndexHtml() {
+    async transformIndexHtml() {
       return {
         tags: [
           {
             tag: "script",
             children: await handleCustomVersionLog(),
-            injectTo: 'body'
+            injectTo: "body",
           },
         ],
       };
