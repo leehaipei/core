c1da583f5d48e15b3fa7cf748c8704ce1732fd25
lile <lile@e-lead.cn>
2025-08-13 11:15:27 +0800

    fix(handleCustomVersionLog): 启用核心分支时间查询并修复日志显示
    之前核心分支时间查询被注释掉，导致日志中显示的是字符串'coreBeijingTime'而不是实际时间。现在取消注释并正确使用查询到的时间值。

--- handler/handleCustomVersionLog.js
+++ handler/handleCustomVersionLog.js
@@ -6,12 +6,12 @@ export default function handleCustomVersionLog(packageJson) {
   return new Promise(async (resolve, reject) => {
     let coreBeijingTime = false;
 
-    // // 查询vite分支的信息
-    // const commits = await axios.get('https://api.github.com/repos/leehaipei/core/branches/vite')
-    // const coreTime = commits?.data?.commit?.commit?.author?.date;
-    // coreBeijingTime = moment.utc(coreTime).utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
+    // 查询vite分支的信息
+    const commits = await axios.get('https://api.github.com/repos/leehaipei/core/branches/vite')
+    const coreTime = commits?.data?.commit?.commit?.author?.date;
+    coreBeijingTime = moment.utc(coreTime).utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
 
-    const logStr = `console.groupCollapsed("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%crelease%c${packageJson['last-release-time']}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%cbuilt%c${momentBeijing().format('YYYY-MM-DD HH:mm:ss')}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%ccore%c${'coreBeijingTime'}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.groupEnd();`
+    const logStr = `console.groupCollapsed("%cversion%c${packageJson.version}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%crelease%c${packageJson['last-release-time']}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%cbuilt%c${momentBeijing().format('YYYY-MM-DD HH:mm:ss')}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.log("%ccore%c${coreBeijingTime}", "border-radius: 3px 0 0 3px;background-color:#20232a;color:#61dafb;font-weight:bold;padding:0px 3px;border:1px solid #20232a", "border-radius:0 3px 3px 0;color:#20232a;background-color:#61dafb;font-weight:bold;padding:1px 3px");console.groupEnd();`
 
     resolve(logStr)
   })
