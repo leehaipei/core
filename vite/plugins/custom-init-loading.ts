ad0b4c73bfd8db4b836f6b7dcef20d56a10588cb
lile <lile@e-lead.cn>
2025-09-17 16:36:53 +0800

    refactor(vite): 重构初始化加载插件
    - 优化随机 ID 生成逻辑，使用自定义的 generateRandomString 函数
    - 修改 HTML、CSS 和 JavaScript 中的类名和 ID，使其更加动态和可读
    - 调整样式和脚本标签的结构，提高代码的可维护性

--- vite/plugins/custom-init-loading.ts
+++ vite/plugins/custom-init-loading.ts
@@ -1,36 +1,47 @@
 import handleExtractModuleScriptSrc from "../../handler/handleExtractModuleScriptSrc";
 
+function generateRandomString(length = 6) {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    result += charset[randomIndex];
+  }
+
+  return result;
+}
+
 export default function customLogEmailTag(): any {
   return {
     name: "custom-log-email-tag",
     transformIndexHtml(html: string) {
       const indexJsFileSrc = handleExtractModuleScriptSrc(html);
-      const randomId = Math.random().toString(36).slice(2);
+      const randomId = generateRandomString();
       return {
         tags: [
           {
             tag: "div",
             attrs: {
-              id: `init-spinner-${randomId}`,
-              class: "init-spinner"
+              id: randomId
             },
-            children: '<i class="init-border-primary init-spinner-border"></i><i class="init-border-secondary init-spinner-border"></i>',
+            children: `<i class="${randomId}-primary ${randomId}-border"></i><i class="${randomId}-secondary ${randomId}-border"></i>`,
             injectTo: 'body'
           },
           {
             tag: "script",
             attrs: {
-              init: `init-script-${randomId}`
+              init: `${randomId}-script`
             },
-            children: `new PerformanceObserver((l)=>{l.getEntries().forEach((e)=>{if(e.name.includes("${indexJsFileSrc}")){document.getElementById("init-spinner-${randomId}")?.remove?.();document.querySelector('style[init="init-css-${randomId}"]')?.remove?.();document.querySelector('script[init="init-script-${randomId}"]')?.remove?.()}})}).observe({type:"resource",buffered:true});`,
+            children: `new PerformanceObserver((l)=>{l.getEntries().forEach((e)=>{if(e.name.includes("${indexJsFileSrc}")){document.getElementById("${randomId}").remove();document.querySelector('style[init="${randomId}-css"]').remove();document.querySelector('script[init="${randomId}-script"]').remove()}})}).observe({type:"resource",buffered:true});`,
             injectTo: "body",
           },
           {
             tag: "style",
-            children: `#init-spinner-${randomId}{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);display:inline-flex;flex-direction:column;align-items:center;z-index:8848;width:32px;height:32px}#init-spinner-${randomId} .init-spinner-border{position:absolute;display:inline-block;width:100%;height:100%;border-bottom-color:#939393;border-left-color:#0000;border-right-color:#0000;border-top-color:#0000;border-width:3px;border-radius:3.40282e38px}#init-spinner-${randomId} .init-border-primary{border-style:solid;animation:init-spinner-spin-${randomId} 0.8s infinite}#init-spinner-${randomId} .init-border-secondary{opacity:0.75;border-style:dotted;animation:init-spinner-spin-${randomId} 0.8s linear infinite}@keyframes init-spinner-spin-${randomId}{0%{transform:rotate(0)}100%{transform:rotate(1turn)}}`,
+            children: `#${randomId}{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);display:inline-flex;flex-direction:column;align-items:center;z-index:8848;width:32px;height:32px}#${randomId} .${randomId}-border{position:absolute;display:inline-block;width:100%;height:100%;border-bottom-color:#939393;border-left-color:#0000;border-right-color:#0000;border-top-color:#0000;border-width:3px;border-radius:3.40282e38px}#${randomId} .${randomId}-primary{border-style:solid;animation:${randomId}-spin 0.8s infinite}#${randomId} .${randomId}-secondary{opacity:0.75;border-style:dotted;animation:${randomId}-spin 0.8s linear infinite}@keyframes ${randomId}-spin{0%{transform:rotate(0)}100%{transform:rotate(1turn)}}`,
             injectTo: 'head',
             attrs: {
-              init: `init-css-${randomId}`
+              init: `${randomId}-css`
             }
           },
         ],
