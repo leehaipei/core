ed108ed0659b0956037a8b369449912c5f5b9bb3
lile <li-le@qq.com>
2026-02-24 23:55:41 +0800

    fix(hash): 添加可选链操作符处理可能的crypto未定义情况
    避免在crypto对象不可用时导致的运行时错误，增强代码健壮性。在本地开发时使用ip地址访问的错误。

--- tools/hash/index.js
+++ tools/hash/index.js
@@ -2,7 +2,7 @@ async function hash(dataStr) {
   const encoder = new TextEncoder();
   const data = encoder.encode(dataStr);
   // 使用Web Crypto API进行哈希计算
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashBuffer = await crypto?.subtle?.digest?.("SHA-256", data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   // 将哈希值转换为十六进制字符串
   const hashHex = hashArray
