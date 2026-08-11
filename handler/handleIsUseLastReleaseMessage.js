4803b1e5370446699373352763b23ffcc8d7b7b1
lile <li-le@qq.com>
2026-08-11 23:21:44 +0800

    feat(release script): add option to reuse last commit message
    add a new handler module to get and prompt using last git commit message,
    update the release workflow to let user choose to reuse previous commit info

---
+++ handler/handleIsUseLastReleaseMessage.js
@@ -0,0 +1,10 @@
+import { confirm } from '@inquirer/prompts';
+import shell from 'shelljs';
+
+export default async function handleIsUseLastReleaseMessage() {
+    const lastReleaseMessage = shell.exec('git log -1 --pretty=%s', { silent: true }).stdout.trim();
+    const result = await confirm({ message: `是否使用上一个提交信息: [${lastReleaseMessage}]`, required: true });
+    return new Promise((resolve, reject) => {
+        resolve({ result, lastReleaseMessage })
+    })
+}
--- scripts/release.js
+++ scripts/release.js
@@ -1,11 +1,19 @@
+import handleIsUseLastReleaseMessage from '../handler/handleIsUseLastReleaseMessage.js';
 import handleReleaseMessage from '../handler/handleReleaseMessage.js';
 import handleVersionAndTime from '../handler/handleVersionAndTime.js';
 import handleReleaseGit from '../handler/handleReleaseGit.js';
 import handleReleaseMerge from '../handler/handleReleaseMerge.js';
 
 async function main() {
+  let message = "";
+  const { result, lastReleaseMessage } = await handleIsUseLastReleaseMessage();
 
-  const message = await handleReleaseMessage();
+  if (result) {
+    message = lastReleaseMessage
+  } else {
+    message = await handleReleaseMessage();
+  }
+
   await handleVersionAndTime(message);
   await handleReleaseGit(message);
   await handleReleaseMerge();
