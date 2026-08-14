eda37aa29d9a216374099cf8763a999b2964059c
lile <li-le@qq.com>
2026-08-15 01:52:05 +0800

    fix(handleIsUseLastReleaseMessage): 给复用的提交信息添加release前缀
    在返回的上次提交信息前统一添加[release]前缀，统一提交信息格式

--- handler/handleIsUseLastReleaseMessage.js
+++ handler/handleIsUseLastReleaseMessage.js
@@ -5,6 +5,6 @@ export default async function handleIsUseLastReleaseMessage() {
     const lastReleaseMessage = shell.exec('git log -1 --pretty=%s', { silent: true }).stdout.trim();
     const result = await confirm({ message: `是否使用上一个提交信息: [${lastReleaseMessage}]`, required: true });
     return new Promise((resolve, reject) => {
-        resolve({ result, lastReleaseMessage })
+        resolve({ result, lastReleaseMessage: `[release] ${lastReleaseMessage}` })
     })
 }
